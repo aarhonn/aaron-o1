9856571ed0dd956134c8e55e2e259c7aa1a1f51f
Aaron Tibbitts <partiallyinsane29@gmail.com>
2025-11-04 10:46:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //script.js, created by aaron tibbitts on 10.26.2025
 const API_KEY = "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"; 
-const ENDPOINT = "https://aaron-tibbitts.github.io/aaron-o1/.netlify/functions/chat";
+const ENDPOINT = "https://api.groq.com/openai/v1/chat/completions";
 const MODEL = "groq/compound";
 
 
